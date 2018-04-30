26d372d82f82507295d376c807ac967bbe6cfc00
Paul Eberlein <34835012+rafmagn@users.noreply.github.com>
2018-04-30 08:39:03 +0200

    [master] sanitize GTM noscript include
    prepare popper's popover

--- map/lib/nasty.js
+++ map/lib/nasty.js
@@ -5,8 +5,11 @@
 /** enabling bootstrap tooltips */
 if (false && (typeof $ === 'function')) {
     $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+    $(function () {
+        $('[data-toggle="popover"]').popover();
+    });
 }
 
 // empheremal dummy cookies…
